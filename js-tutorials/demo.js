90cdc5cf7d036726c16101802520496482943156
waseem123 <waseem_6957@yahoo.com>
2026-04-21 18:38:53 +0530

    js tutorials

---
+++ js-tutorials/demo.js
@@ -0,0 +1,15 @@
+let a = "welcome"
+console.log(typeof(a))
+
+a = 10.55
+console.log(typeof(a))
+
+a = true
+console.log(typeof(a))
+
+
+a = [100,200,300,400,500]
+console.log(typeof(a))
+
+a = {name:'Waseem',city:'Solapur'}
+console.log(typeof(a))
